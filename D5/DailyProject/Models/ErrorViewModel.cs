4dfe97fd674fb83110333275d2a9a734172c4aab
BeatriceV96 <beatricevanvitelli1@gmail.com>
2024-06-28 09:34:56 +0200

    1

---
+++ D5/DailyProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DailyProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
