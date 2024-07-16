12743a86a79bd163ac9edb078707b6f2affef878
BeatriceV96 <beatricevanvitelli1@gmail.com>
2024-07-17 00:11:08 +0200

    8o commit

--- Week1/DeliveryService/DataLayer/Services/Models/AuthService.cs
+++ Week1/DeliveryService/DataLayer/Services/Models/AuthService.cs
@@ -1,16 +1,38 @@
 ﻿
 using System.Data.SqlClient;
 
+
 namespace DeliveryService.DataLayer.Services.Models
 {
     public class AuthService : IAuthService
     {
-        private readonly SqlConnection context; //lo importo dopo aver installato il pacchetto System.Data.SqlClient
+        private readonly string connectionString; //lo importo dopo aver installato il pacchetto System.Data.SqlClient
+        private const string LOGIN_COMMAND //mi preparo una stringa di comando per il login
+
+            = "SELECT Username* FROM Users WHERE Username = @user AND Password = @pass"; //cosi posso lavorare su una tabella autenti
+        public AuthService(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("AuthDb"); // Ottiene correttamente la stringa di connessione
+        }
         public ApplicationUser Login(string username, string password)
         {
-            if (username == password)
-                return new ApplicationUser { UserName = username, Password = password };
-            throw new Exception("Utente non autenticato");
+            try
+            {
+                using var conn = new SqlConnection(connectionString); //mi connetto al db
+                conn.Open(); //apro la connessione
+                using var cmd = new SqlCommand(LOGIN_COMMAND, conn); //mi creo un comando
+                cmd.Parameters.AddWithValue("@user", username); //aggiungo il parametro user
+                cmd.Parameters.AddWithValue("@pass", password); //aggiungo il parametro password
+                using var r = cmd.ExecuteReader(); //eseguo il comando
+                if (r.Read()) //se trovo un risultato
+                {
+                    return new ApplicationUser { Password = password, UserName = username }; //ritorno l'utente
+                }
+            }
+            catch (Exception ex)
+            {
+                // Gestire l'eccezione o registrare l'errore
+            } return null;
         }
     }
 }
